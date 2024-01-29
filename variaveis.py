8c146f5299e56c783c0faddf692807cc748fd8a4
andersontecnicoprogramador <anderson.programador@hotmail.com>
2024-01-28 23:03:53 -0300

    segundo commit

---
+++ variaveis.py
@@ -0,0 +1,46 @@
+# > Variáveis
+ 
+# 1. Variáveis
+
+idade = 41 # Criando uma variável
+
+print(idade)
+
+nome = 'Anderson Moreira da Silva'
+
+print(nome)
+
+"""
+Tipos de Variáveis
+
+1. int: números inteiros, ou seja, números sem parte decimal:0, 5, -1, 1000
+2. float: números reais, ou seja, números com parte decimal: 1.0, -2.7, 3.14
+3. str: cadeias de caracteres, ou seja, dados textuais
+4. bool: valores lógicos(booleanos): True ou False
+"""
+
+idade = 41 
+altura = 1.77
+nome = 'Anderson Moreira'
+estudando = True
+
+print(type(idade) )
+print(type(altura) )
+print(type(nome) )
+print(type(estudando) )
+
+
+
+
+
+
+# Obtendo dados do usuario e salvando em variáveis 
+
+linguagem = input('Qual é a linguagem de programação que você está estudando? ')
+
+print('A linguagem que você está estuadando é', linguagem)
+
+
+
+
+# Imprimindo variáveis + Mais sobre a função print
