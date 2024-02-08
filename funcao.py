a2a2df655b182cdfbfb0ec99db74bed2aaad20ab
Anderson Moreira da Silva <anderson.tecnicoprogramdor@hotmail.com>
2024-02-08 16:58:11 -0300

    nono commit funcao

---
+++ funcao.py
@@ -0,0 +1,23 @@
+# > FUNÇÕES
+
+# 1. O que são funções e por que utilizá-las?
+
+# Funções que ja utilizamos anteriomente...
+"""
+print() # - Imprimir uma mensagem(int, float, str) no console (terminal, cmd)
+input() # - Retonar um dado informado pelo usuário (entrada padrão) e pode receber uma string
+len() # - Recebe uma lista e retorna o tamanho o dessa lista
+max() # - Retorna o maior valor de uma lista
+"""
+# 2. Criação de Funções 
+
+# Função inicial
+
+def saudacao():
+    print('Seja bem-vinda(o)!')
+    print('Ola, é um prazer ter você fazendo parte desse curso!')
+
+saudacao()
+
+
+# Função com parâmetros
